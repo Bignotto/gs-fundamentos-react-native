5025d03cc0671933d2756d61621e7e7395aae1ee
Thiago Bignotto <bignotto@gmail.com>
2020-08-15 18:24:35 -0300

    all tests passed 🚀

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,29 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const cartSum = products.reduce(
+      (acc, prod) => acc + prod.quantity * prod.price,
+      0,
+    );
 
-    return formatValue(0);
+    return formatValue(cartSum);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const cartTotalItems = products.reduce(
+      (acc, prod) => acc + prod.quantity,
+      0,
+    );
 
-    return 0;
+    return cartTotalItems;
   }, [products]);
 
   return (
